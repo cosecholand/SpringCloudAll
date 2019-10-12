8aca9f42ab79da1218d244c4026fb82207edbd59
2zheteng <netheartma@gmail.com>
2019-10-12 14:37:25 +0800

    Spring Boot Demo

---
+++ 01.Spring-Boot-Demo/src/main/java/com/w3cjava/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.w3cjava.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@RequestMapping("/hello")
+	public String index() {
+		return "Hello World";
+	}
+	
+	@RequestMapping("/hello1")
+	public String index1() {
+		return "Hello World1";
+	}	
+}
